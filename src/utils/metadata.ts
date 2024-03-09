bd3d4a4e510df06714c209444d6488c500e06a7d
luis <ldanielcolmenaresm@gmail.com>
2024-03-08 20:12:20 -0400

    :recycle: Metadata

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,6 +1,40 @@
-export const metadata = {
+const metadata = {
   home: {
     title: "Genuino Band",
     desc: "Somos una banda musical cristiana de música contemporánea",
+    favicon: "/favicon.ico",
+    author: "Daniel Colmenares",
+    website: "http://dacoapp.com",
+    generator: "Dacoapp.com",
+  },
+  about: {
+    title: "Sobre Nosotros",
+    desc: "Somos una banda musical cristiana de música contemporánea",
+    favicon: "/favicon.ico",
+    author: "Daniel Colmenares",
+    website: "http://dacoapp.com",
+    generator: "Dacoapp.com",
   },
 };
+
+const { home, about } = metadata;
+
+const HomeMeta = {
+  title: home.title,
+  desc: home.desc,
+  favicon: home.favicon,
+  author: home.author,
+  website: home.website,
+  generator: home.generator,
+};
+
+const AboutMeta = {
+  title: about.title,
+  desc: about.desc,
+  favicon: about.favicon,
+  author: about.author,
+  website: about.website,
+  generator: about.generator,
+};
+
+export { HomeMeta, AboutMeta };
