50d6d434074f60bdea287c0ad5eaf9bf189da622
luis <ldanielcolmenaresm@gmail.com>
2024-03-11 16:37:02 -0400

    update links

---
+++ src/utils/config.ts
@@ -0,0 +1,14 @@
+export const links: { title: string; url: string }[] = [
+  {
+    title: "Calendario",
+    url: "https://calendar.google.com",
+  },
+  {
+    title: "Set Lists",
+    url: "https://docs.google.com/spreadsheets/d/1AT9STOkO_mTf6m-uMT2SfPKpr655xYKeUOTW9V8jd8M/edit?usp=sharing",
+  },
+  {
+    title: "Panel Admin",
+    url: "#",
+  },
+];
